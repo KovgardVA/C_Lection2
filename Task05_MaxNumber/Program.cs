1ba49a2f81984cdab56ace43ab9d746ce16fd7cb
avraamovolono <avraamovolono@yandex.ru>
2022-10-04 21:44:18 +0300

    Add Task 05 Max Number project

---
+++ Task05_MaxNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿int numero = new Random().Next(10,10000);
+Console.WriteLine("Ваше число: " + numero);
+
+int maxNumero = numero % 10;
+numero = numero / 10;
+
+while (numero > 0)
+{
+    if (numero % 10 > maxNumero)
+    {
+        maxNumero = numero % 10;
+    }
+    numero = numero / 10;
+}
+
+Console.WriteLine("Максимальная цифра: " + maxNumero);
