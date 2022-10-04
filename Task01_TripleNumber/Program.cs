c46ab707e2e4bf3ccc2e075278242a5fa9147493
avraamovolono <avraamovolono@yandex.ru>
2022-10-04 20:28:23 +0300

    Add Task 01 Triple Number project

---
+++ Task01_TripleNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    int lastNumber = number % 10;
+    Console.WriteLine($"Последняя цифра: {lastNumber}");
+}
+else{
+    Console.WriteLine("Это не трехзначное число!");
+}
