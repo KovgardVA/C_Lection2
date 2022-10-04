df2dd0d5aa4dc0273d32437ba189c6dc79bb7c73
avraamovolono <avraamovolono@yandex.ru>
2022-10-04 20:47:24 +0300

    Add Task 02 Random project

---
+++ Task02_Random/Program.cs
@@ -0,0 +1,13 @@
+﻿int randomNumber = new Random().Next(10,100);
+Console.WriteLine("Ваше число: " + randomNumber);
+
+int firstNumber = randomNumber / 10;
+int secondNumber = randomNumber % 10;
+
+if (firstNumber > secondNumber) {
+    Console.WriteLine ("Наибальшее число: " + firstNumber);
+}
+else 
+{
+    Console.WriteLine ("Наибальшее число: " + secondNumber);
+}
