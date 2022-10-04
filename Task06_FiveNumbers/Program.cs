de12e565def0d6b7cac1e8368684f975d457d946
avraamovolono <avraamovolono@yandex.ru>
2022-10-04 21:55:13 +0300

    Add Task 06 Five Numbers project

---
+++ Task06_FiveNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = new int[5];
+int index = 0, sum = 0;
+
+Console.WriteLine("Введите пять чисел:");
+while(index < array.Length)
+{
+    array[index] = Convert.ToInt32(Console.ReadLine());
+    index++;
+}
+
+for (index = 0; index < array.Length; index++)
+{
+    sum = sum + array[index];
+}
+
+double middle = sum / array.Length;
+
+Console.WriteLine("Сумма: " + sum + " | Среднее: " + sum / array.Length);
