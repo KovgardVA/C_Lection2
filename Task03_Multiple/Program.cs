3cfd7df859e4a5b1b2c8a0dc652f8b5291a5940e
avraamovolono <avraamovolono@yandex.ru>
2022-10-04 21:02:19 +0300

    Add Task 03 Multiple project

---
+++ Task03_Multiple/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Давайте проверим кратность чисел!");
+Console.WriteLine("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber % secondNumber == 0)
+{
+    Console.WriteLine($"Число {firstNumber} кратно числу {secondNumber}");
+}
+else
+{
+    Console.WriteLine("Числа не кратные! Остаток от деления: " + firstNumber % secondNumber);
+}
