1d59d0f5c028a4f273ae0e3f8260436674fb393b
avraamovolono <avraamovolono@yandex.ru>
2022-10-13 20:15:48 +0300

    Add Example 001 Methods project

--- Lection 3/Example001_Methods/Program.cs
+++ Lection 3/Example001_Methods/Program.cs
@@ -33,17 +33,38 @@ int year = Method3();
 Console.WriteLine(year);
 
 // Вид 4:
-string Method4(int count, string text)
-{
-    int i = 0;
-    string result = String.Empty;
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
 
-    while(i < count)
+//     while(i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "лох");
+// Console.WriteLine(res);
+
+string Method41(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
     {
         result = result + text;
-        i++;
     }
     return result;
 }
-string res = Method4(10, "лох");
+string res = Method41(10, "лох");
 Console.WriteLine(res);
+
+for (int i = 2; i < 10; i++)
+{
+    for (int j = 2; j < 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
