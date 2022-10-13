d736dbc3eef491bfd5c57f5f40781ef7ba96cc34
avraamovolono <avraamovolono@yandex.ru>
2022-10-13 17:37:56 +0300

    Add Semi3Task1

---
+++ Seminar 3/Task01/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причём X ≠ 0 или Y ≠ 0 и выдаёт номер четверти плоскости,
+// в которой находится эта точка.
+
+int Prompt(string massage)
+{
+    Console.WriteLine(massage);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+bool ValidateCoord(int xCoord, int yCoord)
+{
+    if (xCoord == 0 || yCoord == 0)
+    {
+        Console.WriteLine("Точка находится на одной из осей.");
+        return false;
+    }
+    return true;
+}
+
+int GetQuarter(int x, int y)
+{
+    if (y > 0 && x > 0)
+    {
+        return 1;
+    }
+    if (y > 0 && x < 0)
+    {
+        return 2;
+    }
+    if (y < 0 && x < 0)
+    {
+        return 3;
+    }
+    return 4;
+}
+
+int x = Prompt("Введите значение X: ");
+int y = Prompt("Введите значение Y: ");
+
+if (!ValidateCoord(x, y))
+{
+    return;
+}
+
+Console.WriteLine($"Точка находится в {GetQuarter(x, y)} четверти.");
