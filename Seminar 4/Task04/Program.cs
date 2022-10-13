7234ea1abff213b646892a2c8372e04c818d4b1b
avraamovolono <avraamovolono@yandex.ru>
2022-10-13 23:08:26 +0300

    Add Task 04

---
+++ Seminar 4/Task04/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов,
+// заполненный нулями и единицами в случайном порядке.
+
+int[] FillArray(int length)
+{
+    int[] collection = new int[length];
+    for(int i = 0; i < collection.Length; i++) collection[i] = new Random().Next(0,2);
+    return collection;
+}
+
+void PrintArrya(int[] col)
+{
+    int count = col.Length;
+    for (int position = 0; position < count; position++) Console.Write(col[position] + " ");
+}
+
+PrintArrya(FillArray(8));
+
+// void FillandPrintArray(int length)
+// {
+//     int[] array = new int[length];
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(0,2);
+//         Console.Write(array[i] + " ");
+//     }
+// }
+
+// FillandPrintArray(8);
