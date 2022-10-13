b463edbbd3c177b3f6194b7e47295710fdf87f66
avraamovolono <avraamovolono@yandex.ru>
2022-10-13 22:28:08 +0300

    Add Task 01

---
+++ Seminar 4/Task01/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход число (А)
+// и выдаёт сумму чисел от 1 до А.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+bool ValidateNumber(int number)
+{
+    if (number < 1)
+    {
+        Console.WriteLine("Ой! Вы ввели не натуральное число.");
+        return false;
+    }
+    return true;
+}
+
+int SumNumbers (int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i ++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int num = Prompt("Введите натуральное число: ");
+
+if(ValidateNumber(num)) Console.WriteLine($"Сумма чисел от 1 до {num}: {SumNumbers(num)}.");
