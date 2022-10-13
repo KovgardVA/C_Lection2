2cc13f45e4f441f9e188602138c764eb82614815
avraamovolono <avraamovolono@yandex.ru>
2022-10-13 22:30:57 +0300

    Add Task 02

---
+++ Seminar 4/Task02/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход число N
+// и выдаёт произведение чисел от 1 до N.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+bool ValidateNumber(int number)
+{
+    if (number < 1)
+    {
+        Console.WriteLine("Ой! Вы ввели не натуральное число.");
+        return false;
+    }
+    return true;
+}
+
+int ProdNumbers (int number)
+{
+    int sum = 1;
+    for (int i = 1; i <= number; i ++)
+    {
+        sum *= i;
+    }
+    return sum;
+}
+
+int num = Prompt("Введите натуральное число: ");
+
+if(ValidateNumber(num)) Console.WriteLine($"Сумма чисел от 1 до {num}: {ProdNumbers(num)}.");
