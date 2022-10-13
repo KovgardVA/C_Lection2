8f984f55d0f09c9869968aa59f5800a7409795ea
avraamovolono <avraamovolono@yandex.ru>
2022-10-13 22:39:02 +0300

    Add Task 01_1

---
+++ Seminar 4/Task01_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдаёт количество цифр в числе.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int CountOfDigit (int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        number /= 10;
+        count++;
+    }
+    return count;
+}
+
+int num = Prompt("Введите число: ");
+Console.WriteLine($"В числе {num} - {CountOfDigit(num)} цифры.");
