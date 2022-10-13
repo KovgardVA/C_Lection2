73a1446d0554f79a6ff4ba572eec727f73bedf15
avraamovolono <avraamovolono@yandex.ru>
2022-10-13 17:55:56 +0300

    Add Semi3Task2

---
+++ Seminar 3/Task02/Program.cs
@@ -0,0 +1,59 @@
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+bool ValidateQuarter(int quarterNumber)
+{
+    if (quarterNumber < 1 || quarterNumber > 4)
+    {
+        Console.WriteLine("Вы ввели неправильное число!");
+        return false;
+    }
+    return true;
+}
+
+// string GetCoords(int quarterNumber)
+// {
+//     if (quarterNumber == 1)
+//     {
+//         return "X (0; +∞), Y (0; +∞)";
+//     }
+//     if (quarterNumber == 2)
+//     {
+//         return "X (-∞; 0), Y (0; +∞)";
+//     }
+//     if (quarterNumber == 3)
+//     {
+//         return "X (-∞; 0), Y (-∞; 0)";
+//     }
+//     return "X (0; +∞), Y (-∞; 0)";
+// }
+
+string GetCoords(int quarterNumber)
+{
+    switch(quarterNumber)
+    {
+        case 1:
+        return "X (0; +∞), Y (0; +∞)";
+        case 2:
+        return "X (-∞; 0), Y (0; +∞)";
+        case 3:
+        return "X (-∞; 0), Y (-∞; 0)";
+        default:
+        return "X (0; +∞), Y (-∞; 0)";
+    }
+}
+
+
+int quarter = Prompt("Введите номер четверти: ");
+
+if(ValidateQuarter(quarter))
+{
+    Console.WriteLine(GetCoords(quarter));
+}
