f4a726e684ea0d044557610fe2e8af17c9b82145
AvraamOvolono <avraamovolono@yandex.ru>
2022-10-28 11:39:12 +0300

    Seminar 7 Task 02 added

--- Seminar 7/Task02/Program.cs
+++ Seminar 7/Task02/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿int[,] CreateArray(int lenRow, int lenColumn)
+{
+    int[,] array = new int[lenRow, lenColumn];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(-5, 5);
+        }
+    }
+    return array;
+}
+
+int[,] SquareEven(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 != 0 && j % 2 != 0)
+            {
+                array[i, j] *= array[i, j];
+            }
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] myArray = CreateArray(4, 4);
+PrintArray(myArray);
+Console.WriteLine();
+SquareEven(myArray);
+PrintArray(myArray);
