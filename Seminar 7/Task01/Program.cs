7f20b6435e58f90d6345f5babf1f411248c21827
AvraamOvolono <avraamovolono@yandex.ru>
2022-10-28 11:24:55 +0300

    Seminar 7 Task 01 added

---
+++ Seminar 7/Task01/Program.cs
@@ -0,0 +1,27 @@
+﻿int[,] CreateArray(int lenRow, int lenColumn)
+{
+    int[,] array = new int[lenRow, lenColumn];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] myArray = CreateArray(3, 4);
+PrintArray(myArray);
